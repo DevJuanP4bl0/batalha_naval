cf932d050456d6dc0fcef124fb93dd318dce14b8
Juan Pablo <juanpablo140204@outlook.com>
2026-06-27 22:19:25 -0300

    Displaying board - exibeTabuleiro

--- Exibicao.py
+++ Exibicao.py
@@ -1,3 +1,5 @@
+from Tabuleiro import Tabuleiro
+
 #caracteres utilizados
 
 def mostrarCaracteresUsados():
@@ -5,4 +7,15 @@ def mostrarCaracteresUsados():
   print('O caractere X é usado para embarcação afundada')
   print('O caractere \u2316 é usado para embarcação parcialmente atingida')
   print('O caractere \u2588 é usado para denotar posição ainda não exploradas do tabuleiro')
-  
+
+def exibeTabuleiro():
+  linhas  = 6
+  colunas = 6 
+  sep = '  '
+  for i in range(linhas):
+    for j in range(colunas):
+        print('\u2588', end=sep)
+    print()
+
+if __name__ == "__main__":
+  exibeTabuleiro()
